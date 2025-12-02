a7f08e5120c418a34b7a83f2ee13c84e862925bc
Samu902 <samuelemarrani@gmail.com>
2025-12-02 09:55:52 +0100

    interfaccia grafica boids

--- main.cpp
+++ main.cpp
@@ -9,44 +9,70 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
+#include "boids.h"
+
 #define windowWidth 1280
 #define windowHeight 720
+
+#define speedUpSimulation 50
 
 int main(int argc, char* argv[]) {
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Boids Simulation");
 
-    const int numBoids = 10; // numero di agenti (boids)
-    sf::VertexArray boids(sf::Quads, numBoids * 4); // ogni boid è un quadrato (4 vertici)
+    const int numBoids = 1000; // numero di agenti (boids)
+    Boid* boids = new Boid[numBoids];
+    sf::VertexArray boidsQuads(sf::Quads, numBoids * 4); // ogni boid è un quadrato (4 vertici)
+    const int quadSize = 3;
 
-    // Popoliamo i vertici con posizioni random (o la tua logica di movimento)
+    // inizializzazione boids
     for (int i = 0; i < numBoids; ++i) {
         // posizione iniziale random
         float x = rand() % windowWidth;
         float y = rand() % windowHeight;
 
-        // disegna i 4 vertici del quadrato (il boid)
-        boids[i * 4].position = sf::Vector2f(x, y);
-        boids[i * 4 + 1].position = sf::Vector2f(x + 10, y);
-        boids[i * 4 + 2].position = sf::Vector2f(x + 10, y + 10);
-        boids[i * 4 + 3].position = sf::Vector2f(x, y + 10);
-
-        // colore dei boids
-        sf::Color boidColor(255, 255, 255);
-        for (int j = 0; j < 4; ++j) {
-            boids[i * 4 + j].color = boidColor;
-        }
+        boids[i].x = x;
+        boids[i].y = y;
+        boids[i].vx = 0;
+        boids[i].vy = 0;
+        boids[i].scout_group = rand() % 2;
+        boids[i].biasval = 0.001f;
     }
 
     // Ciclo principale
-    while (window.isOpen()) {
+    sf::Clock clock;
+    while (window.isOpen())
+    {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        sf::Time deltaTime = clock.restart();
+
+        // aggiorna i boids
+        for (int i = 0; i < numBoids; ++i)
+        {
+            // calcola il nuovo stato del boid
+            update_boid_position(&boids[i], boids, numBoids, deltaTime.asSeconds() * speedUpSimulation);
+
+            float x = boids[i].x;
+            float y = boids[i].y;
+
+            // disegna i 4 vertici del quadrato (il boid)
+            boidsQuads[i * 4].position = sf::Vector2f(x - quadSize * 0.5f, y - quadSize * 0.5f);
+            boidsQuads[i * 4 + 1].position = sf::Vector2f(x + quadSize * 0.5f, y - quadSize * 0.5f);
+            boidsQuads[i * 4 + 2].position = sf::Vector2f(x + quadSize * 0.5f, y + quadSize * 0.5f);
+            boidsQuads[i * 4 + 3].position = sf::Vector2f(x - quadSize * 0.5f, y + quadSize * 0.5f);
+
+            // colore del boid
+            sf::Color boidColor(255, 255, 255);
+            for (int j = 0; j < 4; ++j) {
+                boidsQuads[i * 4 + j].color = boidColor;
+            }
+        }
 
         window.clear();
-        window.draw(boids); // Disegna tutti i quadrati (boids)
+        window.draw(boidsQuads); // disegna tutti i quadrati (boids)
         window.display();
     }
 }
